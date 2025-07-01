6e133e9d1b4ec9a89dd0c69371d8b9f6a98d2ad2
Abhijay-Singh67 <abhijaysingh.1011@gmail.com>
2025-07-01 20:52:38 +0530

    Added functionality to use the info page for editing profile information as well

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -14,3 +14,12 @@ export async function POST(request) {
     await collection.updateOne({email:email},{$set:{upi:data.upi,profile:data.profile,thumbnail:data.profileThumb,first:data.first,last:data.last,about:data.about}})
     return NextResponse.json({ success: true });
 }
+export async function GET(request) {
+    const db = client.db("TipJar")
+    const collection = db.collection("userdata")
+    const cookieStore = cookies();
+    const SID = (await cookieStore).get("SID")?.value
+    const email = (await collection.findOne({ SID: SID })).email;
+    let user = await collection.findOne({email:email})
+    return NextResponse.json({ success: true, upi:user.upi, profile:user.profile, thumbnail:user.thumbnail, first:user.first,last:user.last,about:user.about});
+}
--- app/info/page.js
+++ app/info/page.js
@@ -1,7 +1,7 @@
 "use client"
-import React, { useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useEdgeStore } from '@/lib/edgestore';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const page = () => {
     const [progress, setProgress] = useState(0);
@@ -11,28 +11,47 @@ const page = () => {
     const [url1, setUrl1] = useState("");
     const [thumb1, setThumb1] = useState("");
     const [url2, setUrl2] = useState("");
+    const [formt, setFormt] = useState({first:"",last:"",about:""})
     const [err, setErr] = useState(false);
     const form = useRef();
     const router = useRouter();
+    const [edit, setEdit] = useState(false);
+    const searchParams = useSearchParams();
+    useEffect(() => {
+        (async () => {
+            const edit = searchParams.get('edit')
+            if (edit == "true") {
+                setEdit(true)
+                let req = await fetch("/api/profile");
+                let res = await req.json();
+                console.log(res);
+                setUrl1(res.profile);
+                setThumb1(res.thumbnail);
+                setUrl2(res.upi);
+                setFormt({first:res.first,last:res.last,about:res.about})
+            }
+        })();
+    },[])
+
     const submitProfile = async (e, url, thumb) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         if (url1 !== "" && (formData.get("first").trim()).length !== 0 && (formData.get("last").trim()).length !== 0 && (formData.get("about").trim()).length !== 0) {
-            let data={};
+            let data = {};
             if (url2 === "") {
                 data = {
-                    "first": formData.get("first").trim(),
-                    "last": formData.get("last").trim(),
+                    "first": formt.first.trim(),
+                    "last": formt.last.trim(),
                     "about": formData.get("about").trim(),
                     "profile": url1,
                     "profileThumb": thumb1,
-                    "upi": ""
+                    "upi": url2
                 }
-            }else{
+            } else {
                 data = {
-                    "first": formData.get("first").trim(),
-                    "last": formData.get("last").trim(),
-                    "about": formData.get("about").trim(),
+                    "first": formt.first.trim(),
+                    "last": formt.last.trim(),
+                    "about": formt.about.trim(),
                     "profile": url1,
                     "profileThumb": thumb1,
                     "upi": url2
@@ -52,6 +71,9 @@ const page = () => {
             setUrl2("");
         }
     }
+    const handleChange = (e)=>{
+        setFormt({...formt,[e.target.name]:e.target.value})
+    }
     return (
         <div className='w-[70vw] h-[80vh] bg-[#f3f2f2] flex justify-center items-center my-[5vh] rounded-4xl mx-auto'>
             <div className="font-mono flex flex-col items-center w-[50%] justify-center">
@@ -59,16 +81,16 @@ const page = () => {
                     <h1 className='text-4xl'>Account Information</h1>
                     <form ref={form} onSubmit={(e) => { submitProfile(e) }} className='flex flex-col gap-7 text-lg w-[100%]'>
                         <div className="name flex gap-2">
-                            <input required={true} name="first" type="text" placeholder="First Name" className='bg-white rounded-full py-5 px-5 flex-grow' />
-                            <input required={true} name="last" type="text" placeholder="Last Name" className='bg-white rounded-full py-5 px-5 flex-grow' />
+                            <input required={true} name="first" value={formt.first}  onChange={(e)=>{handleChange(e)}} type="text" placeholder="First Name" className='bg-white rounded-full py-5 px-5 flex-grow' />
+                            <input required={true} name="last" value={formt.last}  onChange={(e)=>{handleChange(e)}} type="text" placeholder="Last Name" className='bg-white rounded-full py-5 px-5 flex-grow' />
                         </div>
-                        <textarea name="about" placeholder='About You' minLength={1} maxLength={350} required={true} className='bg-white rounded-4xl py-5 px-5 flex-grow h-52'></textarea>
+                        <textarea name="about" placeholder='About You' value={formt.about} onChange={(e)=>{handleChange(e)}}  minLength={1} maxLength={350} required={true} className='bg-white rounded-4xl py-5 px-5 flex-grow h-52'></textarea>
                         <div className='flex flex-col gap-3 items-center justify-center text-sm'>
                             <div className="content flex gap-2 items-center justify-center">
                                 <h1 className='text-lg'>Choose a profile picture</h1>
                                 <input type="file" onChange={(e) => {
                                     setFile1(e.target.files?.[0]);
-                                }} name="projectCover" accept="image/" className='mt-1 border border-[#0000004b] rounded-4xl px-2 cursor-pointer' required={true} />
+                                }} name="projectCover" accept="image/" className='mt-1 border border-[#0000004b] rounded-4xl px-2 cursor-pointer' required={!edit} />
                             </div>
                             <div className="content flex gap-2 items-center justify-center">
                                 <h1 className='text-lg'>UPI QR</h1>
@@ -121,7 +143,7 @@ const page = () => {
                             </div>
                             {err && <h1 className='text-red-500'>File upload not finished...</h1>}
                         </div>
-                        <button type='submit' className='cursor-pointer rounded-full bg-red-500 w-max self-center px-5 py-3 text-white'>Proceed</button>
+                        <button type='submit' className='cursor-pointer rounded-full bg-red-500 w-max self-center px-5 py-3 text-white'>Save</button>
                     </form>
                 </div>
             </div>
