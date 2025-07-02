e42174bad6702b9813b6bb35e0ab6b6c81978c15
Abhijay-Singh67 <abhijaysingh.1011@gmail.com>
2025-07-02 16:56:28 +0530

    Fixed page loading issue

--- app/about/page.js
+++ app/about/page.js
@@ -8,7 +8,7 @@ const Page = () => {
         <p className='text-2xl text-center'>TipJar is a crowdfunding platform where anyone can post about their projects and people interested in their projects can support them through &quot;Tips&quot;.</p>
         <p className='text-2xl text-center'>The aim of the website is to provide a platform where creators can share about their projects, ongoing or completed, with a community of more creators or enthusiasts. It&apos;s a platform to showcase your projects and get them funded by other creators interested in your work.</p>
         <p className='text-2xl text-center'>All creators are requested to accept every other creator on the platform so that it may serve its purpose as intended.</p>
-        <h1 className='text-2xl text-center'>Also <span className='text-4xl text-rose-400 stroke'>THANK YOU&excl;&excl;</span> for visiting my website, I really hope you enjoy your stay.</h1>
+        <h1 className='text-2xl text-center'>Also <span className='text-4xl text-rose-400 stroke'>THANK YOU!!</span> for visiting my website, I really hope you enjoy your stay.</h1>
       </div>
     </div>
   )
